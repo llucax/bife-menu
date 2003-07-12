a72d590d572eb831c045a21f139a74a6c629a67f
Leandro Lucarella <llucax@gmail.com>
2003-07-12 21:39:44 +0000

    Removed \n at the end of the file and added a VIM option to keep it as is.

--- xmi2code.tpl.php
+++ xmi2code.tpl.php
@@ -19,72 +19,9 @@
 // | along with Hooks; if not, write to the Free Software Foundation,   |
 // | Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA      |
 // +--------------------------------------------------------------------+
-// | Created: Sun Jun 8 20:03:30 2003                                   |
+// | Created: @@date                                  |
 // | Authors: Leandro Lucarella <luca@lugmen.org.ar>                    |
 // +--------------------------------------------------------------------+
 //
 // $Id$
 //
-
-// +X2C includes
-require_once 'BIFE/Widget.php';
-// ~X2C
-
-// +X2C Class 115 :Menu
-/**
- * Dynamic Menu.
- *
- * @package BIFE_Menu
- * @access public
- */
-class BIFE_Menu_Menu extends BIFE_Widget {
-    // ~X2C
-
-    // +X2C Operation 117
-    /**
-     * Renders the widget using a template returning a string with the results.
-     *
-     * @param  HTML_Template_HIT &$template Template to use to render the widget.
-     *
-     * @return string
-     * @access public
-     */
-    function render(&$template) // ~X2C
-    {
-        trigger_error('Not implemented!', E_USER_WARNING);
-    }
-    // -X2C
-
-    // +X2C Operation 121
-    /**
-     * Constructor.
-     *
-     * @param  array $attrs Attributes.
-     *
-     * @return void
-     * @access public
-     */
-    function BIFE_Menu_Menu($attrs) // ~X2C
-    {
-        trigger_error('Not implemented!', E_USER_WARNING);
-    }
-    // -X2C
-
-    // +X2C Operation 122
-    /**
-     * Constructor.
-     *
-     * @param  array $attrs Attributes.
-     *
-     * @return void
-     * @access public
-     */
-    function __construct($attrs) // ~X2C
-    {
-        trigger_error('Not implemented!', E_USER_WARNING);
-    }
-    // -X2C
-
-} // -X2C Class :Menu
-
-?>
